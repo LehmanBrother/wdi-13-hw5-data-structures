329aeb53a96f363d220fe25e3d7f99b5932fa1d4
LehmanBrother <chrislehman1991@gmail.com>
2018-09-17 18:58:08 -0500

    data types

---
+++ js/app.js
@@ -0,0 +1,34 @@
+//1. Data types
+//light switch: boolean
+let switch = true //true means on, false means off
+//email: string
+let email = "joe@joe.com"
+//spaceship: object
+const spaceship = {
+	hull:,
+	laserBlasters:,
+	tractorBeam:,
+	warpDrive:
+}
+//name list: array
+const nameArray = ["Ashley","Chris","John"]
+//name list w/locations: array of objects
+const nameLocationArray = 
+	[
+		{name: "Ashley",
+		location: "Madison"
+		},
+		{name: "Chris",
+		location: "Boston"}
+	]
+//name w/location and list of shows: array of objects, each w/array
+const nameLocationShowsArray = 
+	[
+		{name: "Ashley",
+		location: "Madison",
+		shows: ["The Colbert Report","Bojack Horseman"]
+		},
+		{name: "Chris",
+		location: "Boston",
+		shows: ["South Park","Planet Earth"]}
+	]
